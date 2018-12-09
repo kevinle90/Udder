fdb2244badf24bd3650fc96f98cc816ceef5ef9e
Kevin Le <Kevin@kevins-macbook-pro.local>
2018-12-08 20:26:31 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Kevin Le on 12/8/18.
+//  Copyright © 2018 Kevin Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
